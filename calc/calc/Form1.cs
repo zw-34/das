7ad2d1e17aecd4cb25ec3c5c2be93a071e4c5002
zw-34 <76392417+zw-34@users.noreply.github.com>
2020-12-20 15:14:47 +0300

    m

---
+++ calc/calc/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public partial class Form1 : Form
+    {
+        public int a, b, operation;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = 1;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = 2;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = 3;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            textBox1.Clear();
+            operation = 4;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            b = int.Parse(textBox1.Text);
+
+            switch (operation)
+            {
+                case 1:
+                    {
+                        int result = a + b;
+                        textBox1.Text = result.ToString();
+                        break;
+                    }
+                case 2:
+                    {
+                        int result = a - b;
+                        textBox1.Text = result.ToString();
+                        break;
+                    }
+                case 3:
+                    {
+                        int result = a * b;
+                        textBox1.Text = result.ToString();
+                        break;
+                    }
+                case 4:
+                    {
+                        int result = a / b;
+                        textBox1.Text = result.ToString();
+                        break;
+                    }
+            }
+        }
+    }
+}
